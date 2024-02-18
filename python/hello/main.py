519871f16ea6ea405e657c883e02759ab3ac6f33
johnnyxcy <johnny.xcy1997@outlook.com>
2024-02-18 09:43:14 +0800

    modify hello:main

--- python/hello/main.py
+++ python/hello/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import pandas as pd
+
+
+def main():
+    print("hello world")
+    df = pd.DataFrame({'a': [1, 2, 3]})
+    print(df)
+
+if __name__ == "__main__":
+    main()
+    print("done")
