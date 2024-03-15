634c466e11cd587ad6ada8b5a81b48fa10d6dcb0
johnnyxcy <johnny.xcy1997@outlook.com>
2024-03-15 10:35:11 +0800

    fix: remove invalid python

--- projects/python/hello/main.py
+++ projects/python/hello/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import statsmodels as sp
 from greet.config import CommonConfiguration
 
 
@@ -8,13 +7,8 @@ def main(config: CommonConfiguration | None = None):
         print("Hello, {}".format(config.who_am_i))
     df = pd.DataFrame({"a": [1, 2, 3]})
     print(df)
-    print(sp)
 
 
 if __name__ == "__main__":
-    lst = []
-    # l.append("1")
-    # l.append(2)
-    wrong_config = CommonConfiguration(who_am_i=1e-4)
     config = CommonConfiguration(who_am_i="johnnyxcy")
     main(config)
